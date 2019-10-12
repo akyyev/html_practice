75e9d6fd40bbc3bd541a8dbac467f69f3c7a84fd
akyyev <akyyevtm@gmail.com>
2019-10-11 22:56:43 -0400

    js file created

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+
+alert("Sahypama hosh geldiniz");
+ var name = prompt("Adynyz?"); 
+ document.write(name);
+
+var a = (5==="5"); // == compares only values, not types. But === compares both.
+document.write(a);
+
+
+ var date = Date();
+ document.write("<br>"+date);
+ var widh = window.innerWidth;
+ document.write("<br>"+widh);
